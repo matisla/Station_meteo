bd051745b325159632d86d1f6b1c2c234ae537cc
matisla <herrmann.matthieu@gmail.com>
2016-11-25 09:31:01 +0100

    Creation du projet

---
+++ Station_meteo/Station_meteo/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Menu = New System.Windows.Forms.MenuStrip()
+        Me.FichierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Menu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Menu
+        '
+        Me.Menu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FichierToolStripMenuItem})
+        Me.Menu.Location = New System.Drawing.Point(0, 0)
+        Me.Menu.Name = "Menu"
+        Me.Menu.Size = New System.Drawing.Size(460, 24)
+        Me.Menu.TabIndex = 0
+        Me.Menu.Text = "MenuStrip1"
+        '
+        'FichierToolStripMenuItem
+        '
+        Me.FichierToolStripMenuItem.Name = "FichierToolStripMenuItem"
+        Me.FichierToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.FichierToolStripMenuItem.Text = "Fichier"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(460, 371)
+        Me.Controls.Add(Me.Menu)
+        Me.MainMenuStrip = Me.Menu
+        Me.Name = "MainForm"
+        Me.Text = "Form1"
+        Me.Menu.ResumeLayout(False)
+        Me.Menu.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Menu As MenuStrip
+    Friend WithEvents FichierToolStripMenuItem As ToolStripMenuItem
+End Class
