2bdb584da6f4e552d8e849f980f3d1849fa37642
matisla <herrmann.matthieu@gmail.com>
2016-11-25 12:11:11 +0100

    #7 Ajout de la carte dans l'interface graphique

--- Station_meteo/Station_meteo/MainForm.Designer.vb
+++ Station_meteo/Station_meteo/MainForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form remplace la méthode Dispose pour nettoyer la liste des composants.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,14 @@ Partial Class MainForm
     'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
     'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
     'Ne la modifiez pas à l'aide de l'éditeur de code.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.Menu = New System.Windows.Forms.MenuStrip()
         Me.FichierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.img_Carte = New System.Windows.Forms.PictureBox()
         Me.Menu.SuspendLayout()
+        CType(Me.img_Carte, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Menu
@@ -32,7 +35,7 @@ Partial Class MainForm
         Me.Menu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FichierToolStripMenuItem})
         Me.Menu.Location = New System.Drawing.Point(0, 0)
         Me.Menu.Name = "Menu"
-        Me.Menu.Size = New System.Drawing.Size(460, 24)
+        Me.Menu.Size = New System.Drawing.Size(1204, 24)
         Me.Menu.TabIndex = 0
         Me.Menu.Text = "MenuStrip1"
         '
@@ -42,17 +45,33 @@ Partial Class MainForm
         Me.FichierToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
         Me.FichierToolStripMenuItem.Text = "Fichier"
         '
+        'img_Carte
+        '
+        Me.img_Carte.Image = CType(resources.GetObject("img_Carte.Image"), System.Drawing.Image)
+        Me.img_Carte.Location = New System.Drawing.Point(12, 27)
+        Me.img_Carte.Name = "img_Carte"
+        Me.img_Carte.Size = New System.Drawing.Size(745, 745)
+        Me.img_Carte.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.img_Carte.TabIndex = 1
+        Me.img_Carte.TabStop = False
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(460, 371)
+        Me.ClientSize = New System.Drawing.Size(1204, 783)
+        Me.Controls.Add(Me.img_Carte)
         Me.Controls.Add(Me.Menu)
         Me.MainMenuStrip = Me.Menu
+        Me.MaximumSize = New System.Drawing.Size(1220, 822)
+        Me.MinimumSize = New System.Drawing.Size(1220, 822)
         Me.Name = "MainForm"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Station Météo"
+        Me.TopMost = True
         Me.Menu.ResumeLayout(False)
         Me.Menu.PerformLayout()
+        CType(Me.img_Carte, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -60,4 +79,5 @@ Partial Class MainForm
 
     Friend WithEvents Menu As MenuStrip
     Friend WithEvents FichierToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents img_Carte As PictureBox
 End Class
